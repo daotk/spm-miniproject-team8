4be048d08914d5f4d831fb21f8bc7cb48c7a0300
daokhau1@gmail.com <daokhau1@gmail.com@82187cf9-38de-40bb-e423-82ab6ae0d793>
2012-11-16 11:59:31 +0000

    Update cusomerManagement

---
+++ trunk/ABC_Logistics_Project/trunk/QuanLyKhachHang/GUI/CustomerManagement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuanLyKhachHang.GUI
+{
+    public partial class CustomerManagement : Form
+    {
+        public CustomerManagement()
+        {
+            InitializeComponent();
+        }
+
+   
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            AddNewCustomer NewCustomer = new AddNewCustomer();
+            NewCustomer.ShowDialog();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            EditCustomer EditCustomer = new EditCustomer();
+            EditCustomer.ShowDialog();
+        }
+
+        private void CustumerManagement_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
