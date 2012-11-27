a34e689c18b0e27ec042891309b0ea2e496c7b16
daokhau1@gmail.com <daokhau1@gmail.com@82187cf9-38de-40bb-e423-82ab6ae0d793>
2012-11-27 14:49:17 +0000

    update form Quanlydanhmuc

---
+++ ABC_Logistics_Project/trunk/QuanLyKhachHang/GUI/QuanLyDanhMuc.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace QuanLyKhachHang.GUI
+{
+    partial class QuanLyDanhMuc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // QuanLyDanhMuc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1018, 375);
+            this.Name = "QuanLyDanhMuc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý danh mục";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
