38c95740371a1587dd63afaaf0e3296dfdfa0a13
daokhau1@gmail.com <daokhau1@gmail.com@82187cf9-38de-40bb-e423-82ab6ae0d793>
2012-11-29 15:09:54 +0000

    update Search Form

--- ABC_Logistics_Project/trunk/QuanLyKhachHang/GUI/Search.cs
+++ ABC_Logistics_Project/trunk/QuanLyKhachHang/GUI/Search.cs
@@ -12,7 +12,10 @@ namespace QuanLyKhachHang.GUI
 {
     public partial class Search : Form
     {
+        public delegate void Editcusdelegate(string item);
+        public event Editcusdelegate submit;
         ABCLogisticEntities context = new ABCLogisticEntities();
+        string strStatusCheck;
         public Search()
         {
             InitializeComponent();
@@ -24,11 +27,68 @@ namespace QuanLyKhachHang.GUI
         /// <param name="e"></param>
         private void txttukhoa_TextChanged(object sender, EventArgs e)
         {
-            string txttext = txttukhoa.Text;
-            var customer = from p in context.KhachHangTas
-                           where p.MaCongTy.Contains(txttext)
-                           select new { p.MaCongTy, p.TenCTyV, p.QuocGiaTa.TenQuocGia, p.TinhThanhTa.TenTinhThanh, p.DiaChi, p.Sdt, p.LinhVucKinhDoanhTa.TenLVKD, p.NhanVienTa.HovTen };
-            grdtimkiem.DataSource = customer.ToList();
+            //khach hang
+            if (strStatusCheck == rdKhachHang.Text)
+            {
+                if (cboTimKiemTheo.Text == "Mã công ty")
+                {
+                    string txttext = txtTimKiem.Text;
+                    var customer = from p in context.KhachHangTas
+                                   where ((p.MaCongTy.Contains(txttext)) && (p.LoaiKhachHang == rdKhachHang.Text))
+                                   select new { p.MaCongTy, p.TenCTyV, p.QuocGiaTa.TenQuocGia, p.TinhThanhTa.TenTinhThanh, p.DiaChi, p.Sdt, p.LinhVucKinhDoanhTa.TenLVKD, p.NhanVienTa.HovTen };
+                    grdtimkiem.DataSource = customer.ToList();
+                }
+                else
+                {
+                    string txttext = txtTimKiem.Text;
+                    var customer = from p in context.KhachHangTas
+                                   where ((p.TenCTyV.Contains(txttext)) && (p.LoaiKhachHang == rdKhachHang.Text))
+                                   select new { p.MaCongTy, p.TenCTyV, p.QuocGiaTa.TenQuocGia, p.TinhThanhTa.TenTinhThanh, p.DiaChi, p.Sdt, p.LinhVucKinhDoanhTa.TenLVKD, p.NhanVienTa.HovTen };
+                    grdtimkiem.DataSource = customer.ToList();
+                }
+            }
+
+            //Agent
+            if (strStatusCheck == rdAgent.Text)
+            {
+                if (cboTimKiemTheo.Text == "Tên công ty")
+                {
+                    string txttext = txtTimKiem.Text;
+                    var customer = from p in context.KhachHangTas
+                                   where ((p.MaCongTy.Contains(txttext)) && (p.LoaiKhachHang == rdAgent.Text))
+                                   select new { p.MaCongTy, p.TenCTyV, p.QuocGiaTa.TenQuocGia, p.TinhThanhTa.TenTinhThanh, p.DiaChi, p.Sdt, p.LinhVucKinhDoanhTa.TenLVKD, p.NhanVienTa.HovTen };
+                    grdtimkiem.DataSource = customer.ToList();
+                }
+                else
+                {
+                    string txttext = txtTimKiem.Text;
+                    var customer = from p in context.KhachHangTas
+                                   where ((p.TenCTyV.Contains(txttext)) && (p.LoaiKhachHang == rdAgent.Text))
+                                   select new { p.MaCongTy, p.TenCTyV, p.QuocGiaTa.TenQuocGia, p.TinhThanhTa.TenTinhThanh, p.DiaChi, p.Sdt, p.LinhVucKinhDoanhTa.TenLVKD, p.NhanVienTa.HovTen };
+                    grdtimkiem.DataSource = customer.ToList();
+                }
+            }
+
+            //Doi tac
+            if (strStatusCheck == rdDoiTacKhachHang.Text)
+            {
+                if (cboTimKiemTheo.Text == "Tên công ty")
+                {
+                    string txttext = txtTimKiem.Text;
+                    var customer = from p in context.KhachHangTas
+                                   where ((p.MaCongTy.Contains(txttext)) && (p.LoaiKhachHang == rdDoiTacKhachHang.Text))
+                                   select new { p.MaCongTy, p.TenCTyV, p.QuocGiaTa.TenQuocGia, p.TinhThanhTa.TenTinhThanh, p.DiaChi, p.Sdt, p.LinhVucKinhDoanhTa.TenLVKD, p.NhanVienTa.HovTen };
+                    grdtimkiem.DataSource = customer.ToList();
+                }
+                else
+                {
+                    string txttext = txtTimKiem.Text;
+                    var customer = from p in context.KhachHangTas
+                                   where ((p.TenCTyV.Contains(txttext)) && (p.LoaiKhachHang == rdDoiTacKhachHang.Text))
+                                   select new { p.MaCongTy, p.TenCTyV, p.QuocGiaTa.TenQuocGia, p.TinhThanhTa.TenTinhThanh, p.DiaChi, p.Sdt, p.LinhVucKinhDoanhTa.TenLVKD, p.NhanVienTa.HovTen };
+                    grdtimkiem.DataSource = customer.ToList();
+                }
+            }
         }
 
         /// <summary>
@@ -38,7 +98,7 @@ namespace QuanLyKhachHang.GUI
         /// <param name="e"></param>
         private void textBox1_MouseClick(object sender, MouseEventArgs e)
         {
-            txttukhoa.SelectAll();
+            txtTimKiem.SelectAll();
         }
 
         /// <summary>
@@ -48,13 +108,50 @@ namespace QuanLyKhachHang.GUI
         /// <param name="e"></param>
         private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-
             string makh = grdtimkiem.CurrentRow.Cells[0].Value.ToString();
-            EditCustomer Fview = new EditCustomer(makh);
-            Fview.ShowDialog();
-          
+            if (this.submit != null)
+            {
+                this.submit(makh);
+            }
             this.Close();
 
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void rdKhachHang_CheckedChanged(object sender, EventArgs e)
+        {
+            strStatusCheck = rdKhachHang.Text;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void rdAgent_CheckedChanged(object sender, EventArgs e)
+        {
+            strStatusCheck = rdAgent.Text;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void rdDoiTacKhachHang_CheckedChanged(object sender, EventArgs e)
+        {
+            strStatusCheck = rdDoiTacKhachHang.Text;
+        }
+        /// <summary>
+        /// Load form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Search_Load(object sender, EventArgs e)
+        {
+            cboTimKiemTheo.SelectedIndex = 0;
+            rdKhachHang.Checked = true;
         }
 
 
