2ed2382159a462bc381c7f941a31f3508a697b33
daokhau1@gmail.com <daokhau1@gmail.com@82187cf9-38de-40bb-e423-82ab6ae0d793>
2012-12-01 16:41:34 +0000

    update solution

--- trunk/ABC_Logistics_Project/trunk/QuanLyKhachHang/GUI/QuanLyDanhMuc.cs
+++ trunk/ABC_Logistics_Project/trunk/QuanLyKhachHang/GUI/QuanLyDanhMuc.cs
@@ -14,6 +14,7 @@ namespace QuanLyKhachHang.GUI
     public partial class QuanLyDanhMuc : Form
     {
         ABCLogisticEntities context = new ABCLogisticEntities();
+        NgoaiTeTa ngoaite = new NgoaiTeTa();
         string StrUsername;
         public QuanLyDanhMuc(string strUsername)
         {
@@ -48,14 +49,104 @@ namespace QuanLyKhachHang.GUI
         private void QuanLyDanhMuc_Load(object sender, EventArgs e)
         {
             lblTenNhanVien.Text = StrUsername;
+
+            //load tab danh muc ngoai te
+            LoadDanhMuc();
          
+        }
+
+        private void LoadDanhMuc()
+        {
+            var ngoaite = from p in context.NgoaiTeTas
+                          select new { p.MaNgoaiTe, p.TenNgoaiTe, p.TiGiaQuyDoi, p.NgayTao, p.NgayCapNhat, p.GhiChu, };
+            dataGridView1.DataSource = ngoaite.ToList();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
+            EnalbleAll();
+            button3.Hide();
+            button5.Hide();
+            //
+            if (button2.Text == "Lưu")
+            {
+                ngoaite.MaNgoaiTe = txtMaNgoaiTe.Text;
+                ngoaite.TenNgoaiTe = txtTenNgoaiTe.Text;
+                ngoaite.TiGiaQuyDoi =  Int32.Parse(txtTiGia.Text);
+                ngoaite.NgayTao = dtNgayTao.Value;
+                ngoaite.GhiChu = txtGhiChu.Text;
+
+                context.NgoaiTeTas.AddObject(ngoaite);
+                int count = context.SaveChanges();
+                if (count > 0)
+                {
+                    MessageBox.Show("Them thanh cong");
+                    LoadDanhMuc();
+                }
+                else
+                {
+                    MessageBox.Show("Them that bai");
+                }
+            }
+            button2.Text = "Lưu";
+            button1.Text = "Hủy bỏ";
+
+        }
+        /// <summary>
+        /// Enabled
+        /// </summary>
+        private void EnalbleAll()
+        {
+            lblMaNgoaiTe.Enabled = true;
+            lblTenNgoaiTe.Enabled = true;
+            lblTiGia.Enabled = true;
+            lblNgayTao.Enabled = true;
+            lblGhiChu.Enabled = true;
+            txtTenNgoaiTe.Enabled = true;
+            txtMaNgoaiTe.Enabled = true;
+            txtTiGia.Enabled = true;
+            txtGhiChu.Enabled = true;
+            dtNgayTao.Enabled = true;
+        }
+        /// <summary>
+        /// button thu 2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (button1.Text == "Hủy bỏ")
+            {
+                HuyBoKhiLuu();
+                button3.Show();
+                button5.Show();
+                button1.Text = "Chỉnh sửa ngoại tệ";
+                button2.Text = "Thêm mới ngoại tệ";
+
+            }
         }
 
+        private void HuyBoKhiLuu()
+        {
+            txtMaNgoaiTe.Text = "";
+            txtTenNgoaiTe.Text = "";
+            txtTiGia.Text = "";
+            txtGhiChu.Text = "";
+            dtNgayTao.Value = DateTime.Today;
+            //disabled
+            lblMaNgoaiTe.Enabled = false;
+            lblTenNgoaiTe.Enabled = false;
+            lblTiGia.Enabled = false;
+            lblNgayTao.Enabled = false;
+            lblGhiChu.Enabled = false;
+            txtTenNgoaiTe.Enabled = false;
+            txtMaNgoaiTe.Enabled = false;
+            txtTiGia.Enabled = false;
+            txtGhiChu.Enabled = false;
+            dtNgayTao.Enabled = false;
+        }
+
+      
       
     }
 }
